d0a2968686d7b221458840fd4f332ca20ff7400a
heypycn <thorben.linzmaier@web.de>
2025-11-24 17:10:03 +0100

    Implement Particle class (#Issue7)

---
+++ src/particle_class.py
@@ -0,0 +1,22 @@
+##particle class
+from dataclasses import dataclass
+@dataclass
+class Particle:
+    position_x: float
+    position_y: float
+    velocity_x: float
+    velocity_y: float
+    particle_type: int
+    color: str
+    ###Returns current position as tuple from called particle
+    def get_position(self) -> tuple[float,float]:
+        return self.position_x, self.position_y
+    ###Need to define actual physics here but I will write a short DUMMY
+    def apply_force(self,force_x:float, force_y: float):
+        self.velocity_x = self.velocity_x + force_x 
+        self.velocity_y = self.velocity_y + force_y
+        pass
+    ###Updates position based on velocity and time passed
+    def update_position(self,dt):
+        self.position_x = self.position_x + (self.velocity_x * dt)
+        self.position_y = self.position_y + (self.velocity_y * dt)
